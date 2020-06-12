e8e4a6292f84f49ea668ec160a70bacd9e47c6f5
MatiGP <mateusz.swieca@o2.pl>
2020-06-13 00:22:35 +0200

    13.06

---
+++ Unusual_Magic_MageJam01_04_2020/Assets/DuckBossMicrowave.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckBossMicrowave : Attack
+{
+    [SerializeField] Transform[] movePoints;
+    [SerializeField] float microwaveSpeed;
+    [SerializeField] float stayTimeOnPoint;
+    [SerializeField] Animator animator;
+    [SerializeField] LineRenderer lineRenderer;
+    bool arrived;
+    int destinationIndex;
+
+
+
+    private void Start()
+    {
+        destinationIndex = Random.Range(0, movePoints.Length);
+    }
+
+    private void Update()
+    {
+        if(transform.position == movePoints[destinationIndex].position)
+        {           
+            StartCoroutine(SelectNewDestination());
+        }
+        Move(destinationIndex);
+    }
+
+    IEnumerator SelectNewDestination()
+    {
+        yield return new WaitForSeconds(stayTimeOnPoint);
+        destinationIndex = Random.Range(0, movePoints.Length);      
+    }
+
+    IEnumerator ShootLaser()
+    {
+        animator.SetTrigger("attack");
+        yield return new WaitForSeconds(1f);
+        
+    }
+
+    private void Move(int destinationIndex)
+    {
+       transform.position = Vector2.MoveTowards(transform.position, movePoints[destinationIndex].position, microwaveSpeed * Time.deltaTime);
+    }
+
+
+
+    public override void DoAttack()
+    {
+        StartCoroutine
+    }
+}
---
+++ Unusual_Magic_MageJam01_04_2020/Assets/RandomPointRangedAttack.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPointRangedAttack : Attack
+{
+    [SerializeField] Transform[] attackPoints;
+
+    [SerializeField] GameObject projectile;
+
+    float currentAttackCooldown = 0f;
+
+    public override void DoAttack()
+    {
+        if (currentAttackCooldown <= 0f)
+        {
+            GameObject go = Instantiate(projectile, attackPoints[Random.Range(0, attackPoints.Length)].position, Quaternion.identity);
+            go.transform.localScale = new Vector3(transform.localScale.x * go.transform.localScale.x, go.transform.localScale.y, go.transform.localScale.z);           
+            currentAttackCooldown = attackCooldown;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (currentAttackCooldown >= 0f)
+        {
+            currentAttackCooldown -= Time.deltaTime;
+        }
+    }
+
+}
